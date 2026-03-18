9801fe198c3eff5dc61742f39c873be89c54bdb3
Salil Rath <165153168+Salil-IND@users.noreply.github.com>
2026-03-18 16:08:38 +0530

    Corrections in base url in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"DailyBibleVerse-MidTermProject-",
+  base:"/DailyBibleVerse-MidTermProject-/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
